813d17f8914d8df96bc744c120f7c6f62eedf527
Nathan Mead <nmead1@msudenver.edu>
2025-02-10 05:55:52 -0700

    initial commit with functional docker container

---
+++ bug-tracker-backend/src/test/java/nathan_mead/bug_tracker/BugTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package nathan_mead.bug_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
